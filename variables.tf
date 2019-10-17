8dfc01daab7a3a99409539c59f45025a0b7e2a58
marcelvn1104 <42050000+marcelvn1104@users.noreply.github.com>
2019-10-17 16:19:36 +1100

    Add networks sub domains

---
+++ variables.tf
@@ -0,0 +1,7 @@
+# ----------------------------------------------------------------------------------------------------------
+#  Azure Provider
+# ----------------------------------------------------------------------------------------------------------
+variable "subscription_id"                 { default = "dummy" }
+variable "client_id"            { default = "dummy" }
+variable "client_secret"       { default = "dummy" }
+variable "tenant_id"       { default = "dummy" }
