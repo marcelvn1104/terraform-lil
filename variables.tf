5f75a69bd4514acd479c9b045c4c48a454f5358a
marcelvn1104 <42050000+marcelvn1104@users.noreply.github.com>
2019-10-21 15:42:26 +1100

    changed variables.tf

--- variables.tf
+++ variables.tf
@@ -5,3 +5,13 @@ variable "subscription_id"                 { default = "dummy" }
 variable "client_id"            { default = "dummy" }
 variable "client_secret"       { default = "dummy" }
 variable "tenant_id"       { default = "dummy" }
+
+
+#Output variable
+output "first_ouptut" {
+  value = "this is the value through execution"
+}
+
+output "aws_cidr_subnet1" {
+  value = "${aws_subnet.subnet1.cidr_block}"
+}
